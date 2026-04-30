7f0df4ace31a4bfd86b42c14cef2a809db41e8f9
Pieupi <upietroguedes@gmail.com>
2026-04-30 02:16:33 -0300

    Update vip.js

--- plugins/vip.js
+++ plugins/vip.js
@@ -1,13 +1,24 @@
-let handler = async (m, { conn, args }) => {
-  let target = m.mentionedJid[0] || m.sender
+let handler = async (m, { conn }) => {
+  const target = m.mentionedJid?.[0] || m.sender
 
+  // garante objeto do usuário
   if (!global.db.data.users[target]) {
     global.db.data.users[target] = {}
   }
 
-  global.db.data.users[target].plano18 = true
+  const user = global.db.data.users[target]
 
-  m.reply('💎 VIP ativado')
+  // 🔥 ATIVA SEU PLANO
+  user.plano18 = true
+
+  // 🔥 SINCRONIZA COM O SISTEMA DO BOT (EVITA "LOVELY")
+  user.premium = true
+  user.premiumTime = Date.now() + (365 * 24 * 60 * 60 * 1000) // 1 ano
+
+  await conn.sendMessage(m.chat, {
+    text: `💎 VIP ativado para @${target.split('@')[0]}`,
+    mentions: [target]
+  }, { quoted: m })
 }
 
 handler.command = ['vip']
