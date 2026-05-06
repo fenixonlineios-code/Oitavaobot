b03c0485367a9de3492277703d330f5227b1dc3c
Pieupi <upietroguedes@gmail.com>
2026-05-06 02:32:47 -0300

    Update audio-estourar.js

--- plugins/audio-estourar.js
+++ plugins/audio-estourar.js
@@ -13,13 +13,13 @@ const MAX_MB = 15
 
 // 🔊 NORMAL: alto + grave, mas mais entendível
 const FILTRO_ESTOURADO =
-  "bass=g=14:f=85:w=0.8," +
+  "bass=g=19:f=80:w=0.9," +
   "equalizer=f=70:width_type=h:width=120:g=10," +
   "equalizer=f=160:width_type=h:width=180:g=6," +
-  "volume=10dB," +
+  "volume=18dB," +
   "acompressor=threshold=-14dB:ratio=2.5:attack=8:release=120," +
   "asoftclip=type=tanh:param=1.6," +
-  "volume=4dB"
+  "volume=8dB"
 
 // 💀 ABSURDO: fica como o normal antigo
 const FILTRO_ABSURDO =
