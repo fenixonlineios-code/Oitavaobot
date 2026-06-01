49f3f2e6a635a5728d4ed2e63fd16a92a47f11df
Pieupi <upietroguedes@gmail.com>
2026-06-01 04:11:59 -0300

    Update message text and add button for URL

--- plugins/mudarsenha.js
+++ plugins/mudarsenha.js
@@ -1,11 +1,15 @@
 let handler = async (m, { conn, usedPrefix }) => {
   await conn.sendMessage(m.chat, {
-    text: '🍃 *MENUS*',
+    text: '*Altere a senha de sua conta no link abaixo*',
     footer: 'OITAVÃO BOT',
     buttons: [
-      { buttonId: `${usedPrefix}menu search`, buttonText: { displayText: '🔍 SEARCH' }, type: 1 },
-      { buttonId: `${usedPrefix}menu sticker`, buttonText: { displayText: '🖼️ STICKER' }, type: 1 },
-      { buttonId: `${usedPrefix}menu8`, buttonText: { displayText: '➡️ MAIS' }, type: 1 }
+{
+  name: 'cta_url',
+  buttonParamsJson: JSON.stringify({
+    display_text: '🫢 Ver fofocas',
+    url: 'https://tpgb.online/Oitavão/Fofocas.html'
+  })
+}
     ],
     headerType: 1
   }, { quoted: m })
