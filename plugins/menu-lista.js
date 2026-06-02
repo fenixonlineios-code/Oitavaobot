f3babb51a26d28c881a4372632c255f2fb275cbd
Pieupi <upietroguedes@gmail.com>
2026-06-02 04:02:59 -0300

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -143,22 +143,26 @@ ${theme.footer} Abra a lista ou use os botões rápidos.
       }
     )
 
-    const msg = generateWAMessageFromContent(
-  m.chat,
-  {
-    viewOnceMessage: {
-      message: {
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: texto
-          }),
+    const msg = generateWAMessageFromContent(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: proto.Message.InteractiveMessage.create({
+            header: proto.Message.InteractiveMessage.Header.create({
+              title: 'OITAVÃO BOT',
+              subtitle: 'Menu principal',
+              hasMediaAttachment: true,
+              ...media
+            }),
 
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: '© OITAVÃO BOT'
-          }),
+            body: proto.Message.InteractiveMessage.Body.create({
+              text: texto
+            }),
 
-          nativeFlowMessage:
-            proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            footer: proto.Message.InteractiveMessage.Footer.create({
+              text: '© OITAVÃO BOT'
+            }),
+
+            nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
               buttons: [
                 {
                   name: 'single_select',
@@ -183,20 +187,14 @@ ${theme.footer} Abra a lista ou use os botões rápidos.
                 }
               ]
             })
-        })
+          })
+        }
       }
-    }
-  },
-  { quoted: m }
-)
+    }, { quoted: m })
 
-await conn.relayMessage(
-  m.chat,
-  msg.message,
-  {
-    messageId: msg.key.id
-  }
-)
+    await conn.relayMessage(m.chat, msg.message, {
+      messageId: msg.key.id
+    })
 
     await m.react(theme.footer)
 
