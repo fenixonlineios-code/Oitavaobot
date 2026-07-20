410f88cf20a455a741fbb37bc34b38e5a3c2d583
fenixonlineios-code <fenixonline.ios@gmail.com>
2026-07-19 23:46:58 -0300

    Update carrosel.js

--- plugins/carrosel.js
+++ plugins/carrosel.js
@@ -1,42 +1,56 @@
-import fs from 'fs'
-
 let handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, {
-  text: "🌸 Oitavão Bot",
-  footer: "Escolha uma categoria",
-  buttons: [
-    {
-      name: "single_select",
-      buttonParamsJson: JSON.stringify({
-        title: "📂 Abrir Menu",
-        sections: [
+    text: '🌸 OITAVÃO BOT',
+    footer: 'Deslize para o lado 👉',
+
+    cards: [
+      {
+        image: {
+          url: 'https://i.ibb.co/5W62jvz2/IMG-9525.jpg'
+        },
+        title: '📜 Menu Principal',
+        caption: 'Comandos principais',
+
+        nativeFlow: [
           {
-            title: "Categorias",
-            rows: [
-              {
-                header: "🌷 Main",
-                title: "Menu Principal",
-                description: "Comandos principais",
-                id: ".menu main"
-              },
-              {
-                header: "🧰 Tools",
-                title: "Ferramentas",
-                description: "Comandos úteis",
-                id: ".menu tools"
-              },
-              {
-                header: "🎵 Audio",
-                title: "Áudio",
-                description: "Downloads e áudio",
-                id: ".menu audio"
-              }
-            ]
+            text: 'Abrir',
+            id: '.menu'
           }
         ]
-      })
-    }
-  ]
-})
+      },
+
+      {
+        image: {
+          url: 'https://i.ibb.co/5W62jvz2/IMG-9525.jpg'
+        },
+        title: '🧰 Ferramentas',
+        caption: 'Utilidades',
+
+        nativeFlow: [
+          {
+            text: 'Abrir',
+            id: '.tools'
+          }
+        ]
+      },
+
+      {
+        image: {
+          url: 'https://i.ibb.co/5W62jvz2/IMG-9525.jpg'
+        },
+        title: '🎵 Downloads',
+        caption: 'Baixar músicas',
+
+        nativeFlow: [
+          {
+            text: 'Abrir',
+            id: '.play'
+          }
+        ]
+      }
+    ]
+  }, { quoted: m })
+}
+
 handler.command = ['carousel']
 export default handler
